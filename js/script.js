9172130928dd1356e13af2cdc4cceb9a4136ba2d
Maximo <mrdenegate@gmail.com>
2026-04-06 20:27:33 -0300

    querySelector

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log("hola mundo")
+// titulo=document.querySelector('h1')
+// console.log(titulo)
+// titulo.innerHTML='<i> este es mi titulo </i>'
+document.querySelector('h1').innerHTML='<i> titulo </i>'
+document.querySelector('#caja').style.background='blue'
+document.querySelector('.texto').innerHTML='nuevo texto'
