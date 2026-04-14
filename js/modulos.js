7dc9011200e14f0e6d2f1eba9f499e6640385de9
maxasaurio <mrdenegate@gmail.com>
2026-04-13 22:18:54 -0300

    comentario

--- js/modulos.js
+++ js/modulos.js
@@ -1,3 +1,21 @@
-const header ='<strong><i> mi 1ra pagia con modulos'
-const nav='<a> '
-const footer='<p>'
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const header = document.querySelector("#header");
+  header.innerHTML = `
+    <h2>Mi Sitio Web</h2>
+  `;
+
+  const nav = document.querySelector("#nav");
+  nav.innerHTML = `
+    <ul>
+      <li><a href="index.html">INICIO</a></li>
+      <li><a href="Productos.html">PRODUCTOS</a></li>
+      <li><a href="servicios.html">SERVICIOS</a></li>
+    </ul>
+  `;
+
+  const footer = document.querySelector("#footer");
+  footer.innerHTML = `
+    <p>&copy; 2026 Mi Sitio Web - Todos los derechos reservados</p>
+  `;
+});
